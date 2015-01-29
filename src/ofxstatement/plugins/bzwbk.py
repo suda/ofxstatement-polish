236dbbb7b2f54ba6007a2cca492ac7da324fcb57
Wojtek Siudzinski <admin@suda.pl>
2015-01-29 11:08:37 +0100

    CSV import

--- src/ofxstatement/plugins/bzwbk.py
+++ src/ofxstatement/plugins/bzwbk.py
@@ -1,37 +1,36 @@
+import csv
 from ofxstatement.plugin import Plugin
-from ofxstatement.parser import StatementParser
-from ofxstatement.statement import StatementLine
+from ofxstatement.parser import CsvStatementParser
+from ofxstatement.statement import StatementLine, Statement
 
 
 class BZWBKPlugin(Plugin):
     """BZWBK plugin
     """
 
-    def getParser(self, filename):
+    def get_parser(self, filename):
         return BZWBKParser(filename)
 
 
-class BZWBKParser(StatementParser):
+class BZWBKParser(CsvStatementParser):
     def __init__(self, filename):
+        self.statement = Statement()
         self.filename = filename
 
-    def parse(self):
-        """Main entry point for parsers
-
-        super() implementation will call to split_records and parse_record to
-        process the file.
-        """
-        with open(self.filename, "r") as f:
-            self.input = f
-            return super(BZWBKParser, self).parse()
+        with open(self.filename, "r", encoding='utf8') as f:
+            self.fin = f.read()
 
     def split_records(self):
-        """Return iterable object consisting of a line per transaction
-        """
-        return []
+        return csv.reader(self.fin, dialect='foo')
 
     def parse_record(self, line):
-        """Parse given transaction line and return StatementLine object
-        """
         print(line)
-        return StatementLine()
+        stmt_line = StatementLine()
+        for field, col in self.mappings.items():
+            if col >= len(line):
+                raise ValueError("Cannot find column %s in line of %s items "
+                                 % (col, len(line)))
+            rawvalue = line[col]
+            value = self.parse_value(rawvalue, field)
+            setattr(stmt_line, field, value)
+        return stmt_line
